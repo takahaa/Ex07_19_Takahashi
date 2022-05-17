bc12b3a2ccd047a15000e50a0d95afa4f3e1a263
Jun Shimura <junshimura@gmail.com>
2022-05-17 15:09:49 +0900

    プロジェクト ファイルを追加します。

---
+++ Ex5_hint/Ex5_hint.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+
+/// <summary>
+/// 物体の定義
+/// </summary>
+class Solid {
+public:
+    virtual double GetVolume() = 0;
+    virtual double GetSurface() = 0;
+};
+
+/// <summary>
+/// 箱型
+/// </summary>
+class Box :Solid {
+private:
+    double width;
+    double height;
+    double depth;
+
+public:
+    Box(
+        double width,   //幅
+        double height,  //高さ
+        double depth) {//奥行
+        this->width = width;
+        this->height = height;
+        this->depth = depth;
+    }
+    double GetVolume() {
+        return width * height * depth;
+    }
+    double GetSurface() {
+        return (width * height + height * depth + depth * width) * 2;
+    }
+
+
+};
+
+int main()
+{
+    Box box{ 3,5,2.5 };
+    cout << "boxの体積=" << box.GetVolume() << endl;
+    cout << "boxの表面積=" << box.GetSurface() << endl;
+}
